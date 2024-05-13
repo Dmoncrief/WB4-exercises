94e7af542ba3f53b12e5ea36988b89d78da23eac
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-12 23:57:25 -0400

    made changes

--- Sorting/products_sorting.js
+++ Sorting/products_sorting.js
@@ -1,1 +1,15 @@
 "use strict"
+
+supplierCode:productNumber-size
+{
+ supplierCode: "someValue",
+ productNumber: "someValue",
+ size: "someValue"
+}
+let partCode1 = "XYZ:1234-L";
+let part1 = parsePartCode(partCode1);
+console.log("Supplier: " + part1.supplierCode +
+ " Product Number: " + part1.productNumber +
+ " Size: " + part1.size);
+
+ 
